800d8277b06856db14e45c52bafe6d3ea3392cc4
Daniel Oluojomu <danvixent@gmail.com>
2022-03-10 02:56:16 +0100

    echo version

--- download-ngrok.sh
+++ download-ngrok.sh
@@ -3,8 +3,10 @@ file="ngrok.zip"
 OS=$(echo $1 | tr '[:upper:]' '[:lower:]')
 
 if [[ "$OS" == *"linux"* ]]; then
+  echo "downloading linux version of ngrok"
   wget -o $file https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 elif [[ "$OS" == *"mac"* ]]; then
+  echo "downloading mac version of ngrok"
   wget -o $file https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip
 else
     exit
