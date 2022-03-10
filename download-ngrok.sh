fa68c91f5a0a341b32f037d33b29fe67dc51bfc2
Daniel Oluojomu <danvixent@gmail.com>
2022-03-10 03:04:46 +0100

    fix

--- download-ngrok.sh
+++ download-ngrok.sh
@@ -1,13 +1,11 @@
-file="ngrok.zip"
-
 OS=$(echo $1 | tr '[:upper:]' '[:lower:]')
 
 if [[ "$OS" == *"linux"* ]]; then
   echo "downloading linux version of ngrok"
-  wget -o $file https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+  wget --output-document=ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 elif [[ "$OS" == *"mac"* ]]; then
   echo "downloading mac version of ngrok"
-  wget -o $file https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip
+  wget --output-document=ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip
 else
     exit
 fi
